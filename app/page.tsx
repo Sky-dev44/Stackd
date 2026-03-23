343225ec78d2fb28e9b09a09980da99769e0a5b5
Sky-dev44 <jagotshantichakma@gmail.com>
2026-03-23 16:28:44 +0600

    added custom css

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-500">Hello World</p>
+    <div className="py-4 ">
+      <Input placeholder="here input" />
+
+      <Checkbox />
+      <div>
+        <Button variant={"elevated"}>I am button</Button>
+      </div>
+
+      <div>
+        <Progress value={50} />
+      </div>
+
+      <div>
+        <Textarea placeholder="here" />
+      </div>
     </div>
   );
 }
